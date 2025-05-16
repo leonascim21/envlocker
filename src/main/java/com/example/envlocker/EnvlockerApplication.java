3434f36e9f58d5a08a505431a4920b3ea8ee3df7
leonascim <leonascim21@gmail.com>
2025-05-15 23:35:16 -0400

    Initial commit

---
+++ src/main/java/com/example/envlocker/EnvlockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.envlocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvlockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnvlockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/envlocker/EnvlockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.envlocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvlockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
