a9ab2f389a89e26ba953de3900a2acec557943e6
leonascim <leonascim21@gmail.com>
2025-05-16 18:05:33 -0400

    design login page

--- frontend/envlocker/src/pages/login.tsx
+++ frontend/envlocker/src/pages/login.tsx
@@ -1,3 +1,25 @@
 export default function SignInPage() {
-  return <h1>Salve galera</h1>;
+  return (
+    <div className="flex h-dvh justify-center items-center">
+      <div className="card w-96 bg-base-200 card-md shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title">Sign In</h2>
+          <form className="flex flex-col gap-3 p-2">
+            <div>
+              <label className="label">Email</label>
+              <input type="email" className="input" />
+            </div>
+
+            <div>
+              <label className="label">Password</label>
+              <input type="password" className="input" />
+            </div>
+          </form>
+          <div className="card-actions">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
